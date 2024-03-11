9562d0e725540dc9cbbe0ab445d0917c2e75e413
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-03-11 20:03:49 +0000

    add feature to get pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import Title from "./components/Title";
 
 function App() {
+    const [pokemons, setPokemons] = useState([]);
+
+    function getPokeData(id) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then((resp) => resp.json())
+            .then((data) => loadPokemonData(data))
+            .catch((err) => console.error(err));
+    }
+
+    function loadPokemonData(pokeData) {
+        const { id, name, sprites } = pokeData;
+        const spriteFront = sprites.front_default;
+        const newPokeData = { id, name, spriteFront };
+
+        setPokemons((prevPokemons) => [...prevPokemons, newPokeData]);
+    }
+
+    useEffect(() => {
+        getPokeData(1);
+    }, []);
+
     return (
         <>
             <Title />
+            <div>{JSON.stringify(pokemons)}</div>
         </>
     );
 }
